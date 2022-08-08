2d4d255b7a8d70bfa7e2177086dca98607b55521
pkilies <Ilieshdn@hotmail.com>
2022-08-08 20:58:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-const { table } = require("console");
-
-
-function buttons(){
-    
-     // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
-
-  // creates a <table> element and a <tbody> element
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-    for (var i = 0; i < 2; i++) {
-        // creates a table row
-        var row = document.createElement("tr");
-    var row = document.createElement("tr")
-    for (var j = 0; j < 2; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode("cell in row "+i+", column "+j);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-
-}};
-
-function ops(){
-
-
+function bouttons(x){
+    var btn = document.createElement("BUTTON");        // Créer un élément <button>
+    var t = document.createTextNode(x);       // Créer un noeud textuel
+    btn.appendChild(t);                                // Ajouter le texte au bouton
+    document.body.appendChild(btn);   
 };
 
+function clavier(){
+    for(i=0; i<10; i++){
+        bouttons(x);
+    }
+}
 
-function screen(){
-    
+function ecran(){
+    document.getElementById("ecran").innerHTML;
 };
+
+function plus(){
+    var btn = document.createElement("BUTTON");        // Créer un élément <button>
+    var t = document.createTextNode("+");       // Créer un noeud textuel
+    btn.appendChild(t);                                // Ajouter le texte au bouton
+    document.body.appendChild(btn);
+     
+}
+
+function moins(){
+    var btn = document.createElement("BUTTON");        // Créer un élément <button>
+    var t = document.createTextNode("-");       // Créer un noeud textuel
+    btn.appendChild(t);                                // Ajouter le texte au bouton
+    document.body.appendChild(btn);
+     
+}
+
+function diviser(){
+    var btn = document.createElement("BUTTON");        // Créer un élément <button>
+    var t = document.createTextNode("/");       // Créer un noeud textuel
+    btn.appendChild(t);                                // Ajouter le texte au bouton
+    document.body.appendChild(btn);
+     
+}
+
+function multiplier(){
+    var btn = document.createElement("BUTTON");        // Créer un élément <button>
+    var t = document.createTextNode("*");       // Créer un noeud textuel
+    btn.appendChild(t);                                // Ajouter le texte au bouton
+    document.body.appendChild(btn);
+     
+}
