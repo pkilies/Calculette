4813c965e4c6c0460c1fc829cc774aef3e327c79
pkilies <Ilieshdn@hotmail.com>
2022-08-08 17:33:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,27 @@ const { table } = require("console");
 
 function buttons(){
     
-    let new = document.createElement(<buttons>0</buttons>);
-    let new = document.createElement(<buttons>1</buttons>);
-    let new = document.createElement(<buttons>2</buttons>);
-    let new = document.createElement(<buttons>3</buttons>);
-    let new = document.createElement(<buttons>4</buttons>);
-    let new = document.createElement(<buttons>5</buttons>);
-    let new = document.createElement(<buttons>6</buttons>);
-    let new = document.createElement(<buttons>7</buttons>);
-    let new = document.createElement(<buttons>8</buttons>);
-    let new = document.createElement(<buttons>9</buttons>);
-};
+     // get the reference for the body
+  var body = document.getElementsByTagName("body")[0];
+
+  // creates a <table> element and a <tbody> element
+  var tbl = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+    for (var i = 0; i < 2; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
+    var row = document.createElement("tr")
+    for (var j = 0; j < 2; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        var cell = document.createElement("td");
+        var cellText = document.createTextNode("cell in row "+i+", column "+j);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+
+}};
 
 function ops(){
 
