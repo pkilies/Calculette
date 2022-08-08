a4e12cf6c9cf6e6f6e8753f6f2904f1bbee75130
pkilies <Ilieshdn@hotmail.com>
2022-08-08 17:10:32 +0200

    .

---
+++ script.js
@@ -0,0 +1,26 @@
+const { table } = require("console");
+
+
+function buttons(){
+    
+    let new = document.createElement(<buttons>0</buttons>);
+    let new = document.createElement(<buttons>1</buttons>);
+    let new = document.createElement(<buttons>2</buttons>);
+    let new = document.createElement(<buttons>3</buttons>);
+    let new = document.createElement(<buttons>4</buttons>);
+    let new = document.createElement(<buttons>5</buttons>);
+    let new = document.createElement(<buttons>6</buttons>);
+    let new = document.createElement(<buttons>7</buttons>);
+    let new = document.createElement(<buttons>8</buttons>);
+    let new = document.createElement(<buttons>9</buttons>);
+};
+
+function ops(){
+
+
+};
+
+
+function screen(){
+    
+};
