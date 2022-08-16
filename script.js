b2acc515a513e98a663506dc3b2e32c20ec00a42
pkilies <Ilieshdn@hotmail.com>
2022-08-16 14:52:44 +0200

    ..

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
 const {
     doc
 } = require("prettier");
-
+const { command } = require("yargs");
+var screen =0;
+console.log(screen);
 function bouttons(x) {
     document.getElementById("ecran").innerHTML += x;
 };
 
-/*function ecran(){
-    document.getElementById("ecran").innerHTML;
-};*/
-
 function plus() {
-    document.getElementById("ecran").innerHTML += "+";
+    var plus = document.getElementById("ecran").innerHTML;
+    parseInt(plus);
+    console.log(plus);
+    var screen=+plus;
+    console.log(screen);
+    return screen;
 }
 
 function moins() {
-    document.getElementById("ecran").innerHTML += "-";
+    var moins = document.getElementById("ecran").innerHTML;
+    parseFloat(moins);
+    var screen  = screen - moins;
+    console.log(screen);
+    return screen;
 
 }
 
 function diviseur() {
-    document.getElementById("ecran").innerHTML += "/";
+    var division = document.getElementById("ecran").innerHTML;
+    parseFloat(division);
+    screen/division;
+    console.log(screen);
+    return screen;
 }
 
 function fois() {
-    document.getElementById("ecran").innerHTML += "*";
+    var fois = document.getElementById("ecran").innerHTML; 
+    parseInt(fois);
+    screen*fois;
+    return screen;
 }
 
-/*function operations(){
-    var expression = document.getElementById("ecran");
-    if(expression.indexOf('++')
-    ||expression.indexOf('+-')
-    ||expression.indexOf('-+')
-    ||expression.indexOf('+*')
-    ||expression.indexOf('+/')
-    ||expression.indexOf('/+')
-    ||expression.indexOf('--')
-    ||expression.indexOf('-*')
-    ||expression.indexOf('-/')
-    ||expression.indexOf('/-')
-    ||expression.indexOf('//')
-    ||expression.indexOf('**')
-    ||expression.indexOf('/*')
-    ){
-        document.getElementById("ecran").innerHTML = "Erreur";
-    }
-
-}*/
-
 function egal() {
+    document.getElementById("ecran").innerHTML = screen;
+    /*
     var expression = document.getElementById("ecran").innerHTML;
     console.log(expression);
     var resultat = eval(expression);
     console.log(resultat);
-    document.getElementById("ecran").innerHTML = resultat;
+    document.getElementById("ecran").innerHTML = resultat;*/
+    
 
 
 
